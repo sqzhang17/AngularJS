d4cdb80bcac26cfb10b6fd79eda9da8b85b63c39
sqzhang17 <sqzhang17@gmail.com>
2015-09-04 21:31:58 -0700

    change array of object to product

--- FirstApp/js/controllers/MainController.js
+++ FirstApp/js/controllers/MainController.js
@@ -9,9 +9,18 @@ app.controller('MainController', ['$scope', function($scope) {
   Then in the view, 
   you used dot notation to display the values.
   */
-  $scope.product={
-  	name: 'The Book of Trees',
-    price: 19,
-    pubdate: new Date('2014', '03', '08')
-  }
+  $scope.product=[
+    { 
+      name: 'The Book of Trees', 
+      price: 19, 
+      pubdate: new Date('2014', '03', '08'), 
+      cover: 'img/the-book-of-trees.jpg' 
+    }, 
+    { 
+      name: 'Program or be Programmed', 
+      price: 8, 
+      pubdate: new Date('2013', '08', '01'), 
+      cover: 'img/program-or-be-programmed.jpg' 
+    } 
+  ]
 }]);

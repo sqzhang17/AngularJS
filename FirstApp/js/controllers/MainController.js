7c246f1cc1140521f9aa08be7ed0afdf67909438
sqzhang17 <sqzhang17@gmail.com>
2015-09-04 21:04:38 -0700

    adding comment in MainController.js

--- FirstApp/js/controllers/MainController.js
+++ FirstApp/js/controllers/MainController.js
@@ -1,6 +1,14 @@
 app.controller('MainController', ['$scope', function($scope) {
   $scope.title = 'my name is Shiqi Zhang'; 
   $scope.promo = 'my phone is 555-5555';
+
+  /*In the controller, 
+  you used an object to group together 
+  related data about a product. 
+
+  Then in the view, 
+  you used dot notation to display the values.
+  */
   $scope.product={
   	name: 'The Book of Trees',
     price: 19
